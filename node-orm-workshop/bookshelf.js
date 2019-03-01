fc96b605ca2e74bbf6cb550ec35e0e21f1dfa20e
AndresDPG <32043493+AndresDPG@users.noreply.github.com>
2019-03-01 11:53:30 -0500

    *First Commit

---
+++ node-orm-workshop/bookshelf.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var knex = require('knex')(require('./knexfile'));
+var bookshelf = require('bookshelf')(knex);
+
+module.exports = bookshelf;
---
+++ node-orm-workshop/models/contact.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var bookshelf = require('../bookshelf');
+var Contact = bookshelf.Model.extend({
+    tableName: 'contacts',
+});
+
+module.exports = Contact;
